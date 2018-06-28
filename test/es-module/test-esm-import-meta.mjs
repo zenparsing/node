fbe72c61afba8b889caef96c12e4ab80adf314c7
Myles Borins <mylesborins@google.com>
2018-06-27 22:51:38 -0400

    modules: enforce extensions and disallow directories
    this build on top of the limitations of the package name map proposal.
    It enforces the use of file extensions and doesn't support index.js
    or package.json resolution for the default ESM loader implementation.
    
    The current approach is naive and has lots of room for improvement
    but this can serve as a base for discussion
    
    Refs: https://github.com/domenic/package-name-maps

--- test/es-module/test-esm-import-meta.mjs
+++ test/es-module/test-esm-import-meta.mjs
@@ -1,6 +1,7 @@
 // Flags: --experimental-modules
+/* eslint-disable node-core/required-modules */
 
-import '../common';
+import '../common/index.mjs';
 import assert from 'assert';
 
 const fixtures = import.meta.require('../common/fixtures');
